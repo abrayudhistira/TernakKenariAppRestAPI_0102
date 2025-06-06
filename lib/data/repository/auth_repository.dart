9b6d6dc447a375a660fe2c6bf307ee72a74661be
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-06-06 23:09:42 +0700

    correcting import

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -1,26 +1,59 @@
+import 'dart:convert';
+
+import 'package:canary_template/data/model/request/auth/loginRequestModel.dart';
+import 'package:canary_template/data/model/request/auth/registerRequestModel.dart';
+import 'package:canary_template/data/model/response/auth_response_model.dart';
 import 'package:canary_template/services/services_http_client.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AuthRepository {
-
-  final ServicesHttpClient _servicesHttpClient;
+  final ServicesHttpClient _servicehttpClient;
   final secureStorage = FlutterSecureStorage();
 
-  AuthRepository(this._servicesHttpClient);
-  // This class will handle authentication-related operations.
-  // For example, it could include methods for signing in, signing out, and checking authentication status.
+  AuthRepository(this._servicehttpClient);
 
-  Future<void> signIn(String email, String password) async {
-    // Implement sign-in logic here
+  Future<Either<String, AuthResponseModel>> login(LoginRequestModel requestModel) async {
+    try {
+      final response = await _servicehttpClient.post(
+        "Login",
+        requestModel.toMap(),
+      );
+      final jsonResponse = json.decode(response.body);
+      if (response.statusCode == 200) {
+        final loginResponse = AuthResponseModel.fromMap(jsonResponse);
+        await secureStorage.write(
+          key: "AuthToken",
+          value: loginResponse.user?.token,
+        );
+        await secureStorage.write(
+          key: "UserId",
+          value: loginResponse.user?.id.toString(),
+        );
+        return Right(loginResponse);
+      } else {
+        return Left(jsonResponse['error'] ?? 'Login failed');
+      }
+    } catch (e) {
+      return Left("An error occurred while logging in: $e");
+    }
   }
 
-  Future<void> signOut() async {
-    // Implement sign-out logic here
+  Future<Either<String, String>> register(RegisterRequestModel requestModel) async {
+    try {
+      final response = await _servicehttpClient.post(
+        "Register",
+        requestModel.toMap(),
+      );
+      final jsonResponse = json.decode(response.body);
+      final registerResponse = jsonResponse['message'] as String;
+      if (response.statusCode == 201) {
+        return Right(registerResponse);
+      } else {
+        return Left(jsonResponse['error'] ?? 'Registration failed');
+      }
+    } catch (e) {
+      return Left("An error occurred while registering: $e");
+    }
   }
-
-  Future<bool> isAuthenticated() async {
-    // Check if the user is authenticated
-    return false; // Placeholder return value
-  }
-  
 }
