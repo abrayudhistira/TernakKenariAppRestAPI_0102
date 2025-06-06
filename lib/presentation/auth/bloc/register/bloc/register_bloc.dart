5b1a95e61df99899a6c77a89371a94f925de0955
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-06-06 23:13:45 +0700

    implement register

--- lib/presentation/auth/bloc/register/bloc/register_bloc.dart
+++ lib/presentation/auth/bloc/register/bloc/register_bloc.dart
@@ -1,13 +1,28 @@
 import 'package:bloc/bloc.dart';
+import 'package:canary_template/data/model/request/auth/registerRequestModel.dart';
+import 'package:canary_template/data/repository/auth_repository.dart';
 import 'package:meta/meta.dart';
 
 part 'register_event.dart';
 part 'register_state.dart';
 
 class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
-  RegisterBloc() : super(RegisterInitial()) {
-    on<RegisterEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final AuthRepository authRepository;
+  RegisterBloc({required this.authRepository}) : super(RegisterInitial()) {
+    on<RegisterRequested>(_onRegisterRequested);
+  }
+
+  Future<void> _onRegisterRequested(
+    RegisterRequested event,
+    Emitter<RegisterState> emit,
+  ) async {
+    emit (RegisterLoading());
+
+    final result = await authRepository.register(event.requestModel);
+
+    result.fold(
+      (l) => emit(RegisterFailure(error: l)),
+      (r) => emit(RegisterSuccess(message: r)),
+    );
   }
 }
