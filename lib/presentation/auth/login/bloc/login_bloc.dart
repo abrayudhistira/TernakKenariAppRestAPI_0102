4fc31387276e0ab5e2d6edf28edae98f22121460
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-06-06 23:03:31 +0700

    create login bloc

---
+++ lib/presentation/auth/login/bloc/login_bloc.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'login_event.dart';
+part 'login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final AuthRepository authRepository;
+
+  LoginBloc({required this.authRepository}) : super(LoginInitial()) {
+    on<LoginRequested>(_onLoginRequested);
+  }
+
+  Future<void> _onLoginRequested(
+    LoginRequested event,
+    Emitter<LoginState> emit,
+  ) async {
+    emit(LoginLoading());
+    
+    final result = await authRepository.login(event.requestModel);
+
+    result.fold(
+      (l) => emit(LoginFailure(error: l)),
+      (r) => emit(LoginSuccess(responseModel: r as AuthResponseModel)),
+    );
+  }
+}
